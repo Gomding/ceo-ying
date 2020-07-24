94096f9cb91fee9536ec6863a51b94a3724c4672
Gomding <backchida@gmail.com>
2020-07-25 07:54:52 +0900

    프로젝트 대공사 시작

---
+++ src/main/java/com/hululuuuu/ceoying/CeoYingApplication.java
@@ -0,0 +1,13 @@
+package com.hululuuuu.ceoying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeoYingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeoYingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hululuuuu/ceoying/CeoYingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hululuuuu.ceoying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeoYingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
